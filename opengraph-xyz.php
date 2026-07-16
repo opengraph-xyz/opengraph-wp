9c528588e113eccb194852554efe038856d090f4
Antonio Schmitter <antonio@excyted.io>
2026-07-16 17:23:07 +0200

    chore: update version to 1.5.7 in plugin and readme

--- opengraph-xyz.php
+++ opengraph-xyz.php
@@ -3,7 +3,7 @@
 Plugin Name: OpenGraph.xyz
 Plugin URI: https://github.com/opengraph-xyz/opengraph-wp
 Description: Dynamic Open Graph images for your website
-Version: 1.5.6
+Version: 1.5.7
 Author: OpenGraph.xyz
 Author URI: https://opengraph.xyz
 License: GPLv3
